80a72937b0bca7cbd22d567d89ddbb6e056fb14d
andrpet <andrpet@gmail.com>
2018-12-03 12:04:43 +0100

    Deep2 Client

--- js/deepClient.js
+++ js/deepClient.js
@@ -1,114 +1,51 @@
-/*
- @license
- The MIT License (MIT)
-
- Copyright (c) 2015 Dipartimento di Informatica - Universit� di Salerno - Italy
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
-
-/**
- * Developed by :
- * ROUTE-TO-PA Project - grant No 645860. - www.routetopa.eu
- *
- */
-
-var ComponentService =
+!function()
 {
-	deep_url:"",
-    cache:[],
-
-    getComponent: function(params) {
-        var request = this.getXMLHttpRequest();
-        var component = params.component;
-        var cache = this.cache;
-
-        request.onreadystatechange = function ()
-        {
-            if (request.readyState == 4) {
-                try {
-                    var resp = JSON.parse(request.response);
-                    var link = '<link rel="import" href="' + resp.bridge_link + resp.component_link + '">';
-                    $('head').append(link);
-                    create_datalet_html_code();
-                } catch (e) {
-                    var resp = {
-                        status: 'error',
-                        data: 'Unknown error occurred: [' + request.response + ']'
-                    };
-                }
-            }
-        };
-
-        var create_datalet_html_code = function()
-        {
-            //Build datalet injecting html code
-            var datalet_code = '<' + params.component;
-            var keys = Object.keys(params.params);
-            for (var i = 0; i < keys.length; i++) {
-                datalet_code += ' ' + keys[i] + '=\'' + params.params[keys[i]].toString().replace(/\'/g, "&#39;") + '\'';
-            }
-
-            datalet_code += "></" + params.component + ">";
-
-            (params.placeHolder.constructor === HTMLElement || params.placeHolder.constructor === HTMLDivElement) ? $(params.placeHolder).html(datalet_code) : /*Injection from Web Component*/
-                $("#" + params.placeHolder).html(datalet_code);/*Injection from a static web page*/
-        };
-
-        //if (!this.isRegistered(component))
-        if (!cache[this.deep_url + component])
-        {
-            //console.log('ciao');
-            request.open('GET', this.deep_url + component);
-            request.send();
-            cache[this.deep_url + component] = true;
-        }
-        else
-        {
-            create_datalet_html_code();
-        }
-    },
-
-    getXMLHttpRequest: function ()
+    let getComponent = function(params)
     {
-        if (window.XMLHttpRequest) {
-            return new XMLHttpRequest();
-        }
-        else {
-            try {
-                return new ActiveXObject("MSXML2.XMLHTTP.3.0");
-            }
-            catch(ex) {
-                return null;
-            }
-        }
-    },
+        create_link(params.component);
+        create_datalet(params);
+    };
 
-    isRegistered: function(name) {
-        //return document.createElement(name).constructor !== HTMLElement;
-
-        try {
-            document.registerElement(name);
-            return false;
-        } catch(e) {
-            return true;
+    let create_link = function (component)
+    {
+        if(!is_registered(component))
+        {
+            let type = component.substring(component.lastIndexOf('-')+1, component.length);
+            let link = document.createElement('link');
+            link.setAttribute("rel", "import");
+            link.setAttribute("href", `${window.ComponentService.components_url}${type}s/${component}/${component}.html`);
+            document.getElementsByTagName('head')[0].appendChild(link);
         }
+    };
 
-    }
-};
+    let create_datalet = function(params)
+    {
+        try
+        {
+            let datalet = document.createElement(params.component);
+            let keys = Object.keys(params.params);
+
+            for (let i = 0; i < keys.length; i++)
+                datalet.setAttribute(keys[i], parse_attribute(params.params[keys[i]]));
+
+            (params.placeHolder.constructor === HTMLElement || params.placeHolder.constructor === HTMLDivElement) ? (params.placeHolder.innerHTML = datalet.outerHTML) : /*Injection from Web Component*/
+                (document.getElementById(params.placeHolder).innerHTML = datalet.outerHTML); /*Injection from a static web page*/
+
+        }catch(e){}
+    };
+
+    let parse_attribute = function (attribute)
+    {
+        return (typeof attribute === 'object' ? JSON.stringify(attribute) : attribute)
+    };
+
+    let is_registered = function (component)
+    {
+        return document.querySelector(`link[href*="${component}"]`)
+    };
+
+    window.ComponentService = {};
+    window.ComponentService.components_url = ''; // ODE.deep_components
+    window.ComponentService.getComponent = getComponent;
+
+}();
